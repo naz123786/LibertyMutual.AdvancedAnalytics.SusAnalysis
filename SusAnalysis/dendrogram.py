4a3345cbdc8723ed19dbe2b78e383eb2d7874e38
tlagrone <travis.lagrone@trintech.com>
2018-12-02 17:48:28 -0600

    Rename 'sus_df' to 'predictors'

--- SusAnalysis/dendrogram.py
+++ SusAnalysis/dendrogram.py
@@ -18,29 +18,29 @@ INPUT_PATH = Path('')
 #%% Read Data
 raw_df = pd.read_excel(INPUT_PATH)
 
-sus_df = raw_df[['Region',
-                 'Top_Account_Nme',
-                 'Claim_Office_Cde',
-                 'Marital_Status_Cde',
-                 'Primary_Part_Of_Body_Cde',
-                 'Trtm_Cde',
-                 'Tenure_Bucket',
-                 'Report_Bucket',
-                 'Primary_Nature_Of_Injury_Cde',
-                 'Occurrence_Cde',
-                 'Age_Bucket']]
+predictors = raw_df[['Region',
+                     'Top_Account_Nme',
+                     'Claim_Office_Cde',
+                     'Marital_Status_Cde',
+                     'Primary_Part_Of_Body_Cde',
+                     'Trtm_Cde',
+                     'Tenure_Bucket',
+                     'Report_Bucket',
+                     'Primary_Nature_Of_Injury_Cde',
+                     'Occurrence_Cde',
+                     'Age_Bucket']]
 
-sus_df['Region'] = sus_df['Region'].astype(str)
-sus_df['Top_Account_Nme'] = sus_df['Top_Account_Nme'].astype(str)
-sus_df['Claim_Office_Cde'] = sus_df['Claim_Office_Cde'].astype(str)
-sus_df['Marital_Status_Cde'] = sus_df['Marital_Status_Cde'].astype(str)
-sus_df['Primary_Part_Of_Body_Cde'] = sus_df['Primary_Part_Of_Body_Cde'].astype(str)
-sus_df['Trtm_Cde'] = sus_df['Trtm_Cde'].astype(str)
-sus_df['Tenure_Bucket'] = sus_df['Tenure_Bucket'].astype(str)
-sus_df['Report_Bucket'] = sus_df['Report_Bucket'].astype(str)
-sus_df['Primary_Nature_Of_Injury_Cde'] = sus_df['Primary_Nature_Of_Injury_Cde'].astype(str)
-sus_df['Occurrence_Cde'] = sus_df['Occurrence_Cde'].astype(str)
-sus_df['Age_Bucket'] = sus_df['Age_Bucket'].astype(str)
+predictors['Region'] = predictors['Region'].astype(str)
+predictors['Top_Account_Nme'] = predictors['Top_Account_Nme'].astype(str)
+predictors['Claim_Office_Cde'] = predictors['Claim_Office_Cde'].astype(str)
+predictors['Marital_Status_Cde'] = predictors['Marital_Status_Cde'].astype(str)
+predictors['Primary_Part_Of_Body_Cde'] = predictors['Primary_Part_Of_Body_Cde'].astype(str)
+predictors['Trtm_Cde'] = predictors['Trtm_Cde'].astype(str)
+predictors['Tenure_Bucket'] = predictors['Tenure_Bucket'].astype(str)
+predictors['Report_Bucket'] = predictors['Report_Bucket'].astype(str)
+predictors['Primary_Nature_Of_Injury_Cde'] = predictors['Primary_Nature_Of_Injury_Cde'].astype(str)
+predictors['Occurrence_Cde'] = predictors['Occurrence_Cde'].astype(str)
+predictors['Age_Bucket'] = predictors['Age_Bucket'].astype(str)
 
 
 #%% Rank Feature
