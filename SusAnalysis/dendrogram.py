db26d111e26324d7ecb2cc2ab060cfefad62b227
tlagrone <travis.lagrone@trintech.com>
2018-12-02 17:53:09 -0600

    Refactor column subsetting

--- SusAnalysis/dendrogram.py
+++ SusAnalysis/dendrogram.py
@@ -17,6 +17,20 @@ INPUT_PATH = Path('')
 
 #%% Read Data
 raw_df = pd.read_excel(INPUT_PATH)
+raw_df = raw_df[['Region',
+                 'Top_Account_Nme',
+                 'Claim_Office_Cde',
+                 'Marital_Status_Cde',
+                 'Primary_Part_Of_Body_Cde',
+                 'Trtm_Cde',
+                 'Tenure_Bucket',
+                 'Report_Bucket',
+                 'Primary_Nature_Of_Injury_Cde',
+                 'Occurrence_Cde',
+                 'Age_Bucket',
+                 'Year',
+                 'POP']]
+raw_df = raw_df.astype(str)
 
 predictors = raw_df[['Region',
                      'Top_Account_Nme',
@@ -30,19 +44,7 @@ predictors = raw_df[['Region',
                      'Occurrence_Cde',
                      'Age_Bucket']]
 
-regressors = raw_df[['Year', 'POP']]
-
-predictors['Region'] = predictors['Region'].astype(str)
-predictors['Top_Account_Nme'] = predictors['Top_Account_Nme'].astype(str)
-predictors['Claim_Office_Cde'] = predictors['Claim_Office_Cde'].astype(str)
-predictors['Marital_Status_Cde'] = predictors['Marital_Status_Cde'].astype(str)
-predictors['Primary_Part_Of_Body_Cde'] = predictors['Primary_Part_Of_Body_Cde'].astype(str)
-predictors['Trtm_Cde'] = predictors['Trtm_Cde'].astype(str)
-predictors['Tenure_Bucket'] = predictors['Tenure_Bucket'].astype(str)
-predictors['Report_Bucket'] = predictors['Report_Bucket'].astype(str)
-predictors['Primary_Nature_Of_Injury_Cde'] = predictors['Primary_Nature_Of_Injury_Cde'].astype(str)
-predictors['Occurrence_Cde'] = predictors['Occurrence_Cde'].astype(str)
-predictors['Age_Bucket'] = predictors['Age_Bucket'].astype(str)
+regressors = raw_df[['POP']]
 
 
 #%% Rank Feature
