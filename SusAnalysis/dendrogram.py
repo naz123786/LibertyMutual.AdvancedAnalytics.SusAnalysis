40343053272710753b268cc505dc59c4831d3cf6
naz123786 <43978210+naz123786@users.noreply.github.com>
2018-12-02 16:41:21 -0600

    Update dendrogram.py

--- SusAnalysis/dendrogram.py
+++ SusAnalysis/dendrogram.py
@@ -8,16 +8,18 @@ import pandas as pd
 INPUT_PATH = Path('')
 
 #%% Import Dataset
-SUS = pd.read_excel(INPUT_PATH,
+SUS = pd.read_excel('C:\\Users\\n0253665\\Desktop\\SUS_EDA.xlsx', 
                     dtype={"Region",
                     "Top_Account_Nme",
-                    "Claim_Office_Cde
-Marital_Status_Cde
-Primary_Part_Of_Body_Cde
-Trtm_Cde
-Tenure_Bucket
-Report_Bucket
-Primary_Nature_Of_Injury_Cde
-Occurrence_Cde
-Age_Bucket
+                    "Claim_Office_Cde",
+                    "Marital_Status_Cde",
+                    "Primary_Part_Of_Body_Cde",
+                    "Trtm_Cde",
+                    "Tenure_Bucket",
+                    "Report_Bucket",
+                    "Primary_Nature_Of_Injury_Cde",
+                    "Occurrence_Cde",
+                    "Age_Bucket",
+                    "Year",
+                    "POP"
 })
