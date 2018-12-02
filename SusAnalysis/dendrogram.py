c92a71177e50fedd644179bc5e6531ca4a00d885
naz123786 <43978210+naz123786@users.noreply.github.com>
2018-12-02 16:31:27 -0600

    Create dendrogram.py

---
+++ SusAnalysis/dendrogram.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import numpy as np
+import sklearn as sk
+import pandas as pd
+
+#%% Configuration
+INPUT_PATH = Path('')
+
+#%% Import Dataset
+SUS = pd.read_excel(INPUT_PATH,
+                    dtype={"Region",
+                    "Top_Account_Nme",
+                    "Claim_Office_Cde
+Marital_Status_Cde
+Primary_Part_Of_Body_Cde
+Trtm_Cde
+Tenure_Bucket
+Report_Bucket
+Primary_Nature_Of_Injury_Cde
+Occurrence_Cde
+Age_Bucket
+})
