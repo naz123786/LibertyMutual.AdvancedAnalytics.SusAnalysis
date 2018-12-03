9311641795efb20de81f2d428a9fd2a61edcf391
tlagrone <travis.lagrone@trintech.com>
2018-12-02 20:23:52 -0600

    Debug column typing; Clean up variable naming

--- SusAnalysis/dendrogram.py
+++ SusAnalysis/dendrogram.py
@@ -18,39 +18,40 @@ from sklearn.feature_selection import mutual_info_classif
 
 
 #%% Read Data
-raw_df = pd.read_excel(INPUT_PATH)
-raw_df = raw_df[['Region',
-                 'Top_Account_Nme',
-                 'Claim_Office_Cde',
-                 'Marital_Status_Cde',
-                 'Primary_Part_Of_Body_Cde',
-                 'Trtm_Cde',
-                 'Tenure_Bucket',
-                 'Report_Bucket',
-                 'Primary_Nature_Of_Injury_Cde',
-                 'Occurrence_Cde',
-                 'Age_Bucket',
-                 'Year',
-                 'POP']]
-raw_df = raw_df.astype('category')
+data = pd.read_excel(INPUT_PATH)
+data = data[['Region',
+             'Top_Account_Nme',
+             'Claim_Office_Cde',
+             'Marital_Status_Cde',
+             'Primary_Part_Of_Body_Cde',
+             'Trtm_Cde',
+             'Tenure_Bucket',
+             'Report_Bucket',
+             'Primary_Nature_Of_Injury_Cde',
+             'Occurrence_Cde',
+             'Age_Bucket',
+             'Year',
+             'POP']]
+for col in data.columns:
+    data[col] = data[col].astype('category')
 
-predictors = raw_df[['Region',
-                     'Top_Account_Nme',
-                     'Claim_Office_Cde',
-                     'Marital_Status_Cde',
-                     'Primary_Part_Of_Body_Cde',
-                     'Trtm_Cde',
-                     'Tenure_Bucket',
-                     'Report_Bucket',
-                     'Primary_Nature_Of_Injury_Cde',
-                     'Occurrence_Cde',
-                     'Age_Bucket']]
+predictors = data[['Region',
+                   'Top_Account_Nme',
+                   'Claim_Office_Cde',
+                   'Marital_Status_Cde',
+                   'Primary_Part_Of_Body_Cde',
+                   'Trtm_Cde',
+                   'Tenure_Bucket',
+                   'Report_Bucket',
+                   'Primary_Nature_Of_Injury_Cde',
+                   'Occurrence_Cde',
+                   'Age_Bucket']]
 
-regressors = raw_df[['POP']]
+regressor = data['POP']
 
 
 #%% Rank Features (filter approach)
-scores = mutual_info_classif(predictors, regressors, discrete_features=True)
+scores = mutual_info_classif(predictors, regressor, discrete_features=True)
 scores = dict(zip(predictors.columns, scores))
 scores = list(sorted(scores, lambda x: x[1]))
 print(scores)
